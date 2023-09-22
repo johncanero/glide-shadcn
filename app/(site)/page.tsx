cd43e530ea05383550b08b728add97a230c365c4
John Cañero <johncanero.official@gmail.com>
2023-09-23 00:16:04 +0800

    Add Patterns of Front Page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -25,17 +25,40 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="items-center hidden space-x-4 lg:flex">
-                    <Button className="w-auto px-8 py-6 mt-6 rounded-xl bg-zinc-50 hover:bg-zinc-900 hover:text-white">Get GliderSwift</Button>
+                    <Button className="z-10 w-auto px-8 py-6 mt-7 rounded-xl bg-zinc-50 hover:bg-zinc-900 dark:text-black dark:hover:text-white hover:text-white">Get GliderSwift</Button>
                 </div>
             </div>
 
+            {/* Desktop - Arrow */}
+            <div className='hidden lg:absolute lg:block lg:bottom-96 lg:left-0'>
+                <Image
+                    src="/images/patterns/lineWhite.png"
+                    width={250}
+                    height={250}
+                    alt="Line White"
+                    className='mx-auto'
+                />
+            </div>
+
+            {/* Desktop - Arrow */}
             <div className='hidden lg:absolute lg:block lg:bottom-60 lg:right-96'>
                 <Image
                     src="/images/patterns/right-arrowWhite.png"
                     width={500}
                     height={500}
                     alt="Right Arrow"
-                    className='mx-auto opacity-100 md:max-w-2xl lg:max-w-full dark:opacity-70 md:rounded-3xl'
+                    className='mx-auto'
+                />
+            </div>
+
+            {/* Desktop - Circles */}
+            <div className='hidden lg:absolute lg:block lg:bottom-64 lg:right-20'>
+                <Image
+                    src="/images/patterns/white-circles.png"
+                    width={250}
+                    height={250}
+                    alt="White Cirlces"
+                    className='mx-auto'
                 />
             </div>
 
