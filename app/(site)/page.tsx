ae968a508366b02e15aafdee38ef57ae8cf99b15
John Cañero <johncanero.official@gmail.com>
2023-09-22 22:20:37 +0800

    Add Content Text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,15 +3,21 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <div className="relative">
-            <Image
-                src="/images/gliderBackground.jpg"
-                width={1200}
-                height={1000}
-                alt="Glider Background"
-                className='w-full'
-            />
-            <div className="absolute top-44 left-44 text-white p-4 max-w-2xl">
-                <h1 className="text-7xl font-bold">Scooter sharing made simple</h1>
+            <div>
+                <Image
+                    src="/images/gliderBackground.jpg"
+                    width={1400}
+                    height={1000}
+                    alt="Glider Background"
+                    className='max-w-full opacity-100 dark:opacity-70 rounded-3xl'
+                />
+            </div>
+            <div className="absolute max-w-2xl p-4 text-white top-44 left-44">
+                <h1 className="font-bold text-7xl">Scooter sharing made simple</h1>
+                <div className='mt-4'>
+                    <p>Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities.</p>
+                    <p>Use our app</p>
+                </div>
             </div>
         </div>
     )
